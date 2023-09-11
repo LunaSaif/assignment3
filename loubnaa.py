15426651ce5dfe08f9b928b1dc1febf6e0e9f7af
LunaSaif <143600026+LunaSaif@users.noreply.github.com>
2023-09-11 17:36:48 +0800

    Update loubnaa.py

--- loubnaa.py
+++ loubnaa.py
@@ -1,43 +1,51 @@
-import dash
-from dash import dcc, html
 import pandas as pd
 import plotly.express as px
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 
+# Load the dataset
+data_url = "https://raw.githubusercontent.com/LunaSaif/assignment/main/COVID-19%20Coronavirus.csv"
+data = pd.read_csv('covid_data.csv')  
+
+# Initialize the Dash app
 app = dash.Dash(__name__)
-server = app.server
 
-data_url = "https://raw.githubusercontent.com/LunaSaif/assignment3/main/COVID-19 Coronavirus.csv"
-data = pd.read_csv(data_url)
-    # Limit the data to the first 10 rows
-data = data.head(10)
-data2 = data[['Country','Death percentage']]
-data2.head(10)
-
-data3 = data[['Country','Continent','Total Cases','Total Deaths']]
-data3 = data3.sort_values('Total Cases', ascending=False)
-data3.head(10)
-
+# Define the app layout
 app.layout = html.Div([
-    html.H1("Luna Assignment 3"),
-
-    # bar chart
-    dcc.Graph(
-        id='total-cases-bar-chart',
-        figure=px.bar(data3, x='Country', y='Total Cases', title='Total Cases by Country'),
+    html.H1("COVID-19 Data Visualization"),
+    
+    # Dropdown for selecting a country
+    dcc.Dropdown(
+        id='country-dropdown',
+        options=[{'label': country, 'value': country} for country in data['Country,Other']],
+        value='Afghanistan'  # Default selected country
     ),
     
-    # second bar chart
-    dcc.Graph(
-        id='total-deaths-bar-chart',
-        figure=px.bar(data3, x='Country', y='Total Deaths', title='Total Deaths by Country'),
-    ),
-    
-    # Add the pie chart
-    dcc.Graph(
-        id='cases-pie-chart',
-        figure=px.pie(data3, values='Total Cases', names='Continent', title='Cases of Covid-19 by Continent'),
-    ),
+    # Graph to display COVID-19 data
+    dcc.Graph(id='covid-graph'),
 ])
+
+# Define callback to update the graph based on the selected country
+@app.callback(
+    Output('covid-graph', 'figure'),
+    [Input('country-dropdown', 'value')]
+)
+def update_graph(selected_country):
+    # Filter the dataset for the selected country
+    filtered_data = data[data['Country,Other'] == selected_country]
+
+    # Create a bar chart for Total Cases and Total Deaths
+    fig = px.bar(
+        filtered_data,
+        x='Total Cases',
+        y='Total Deaths',
+        title=f"COVID-19 Statistics for {selected_country}",
+        labels={'Total Cases': 'Total Cases', 'Total Deaths': 'Total Deaths'}
+    )
+
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
