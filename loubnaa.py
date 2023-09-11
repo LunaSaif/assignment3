e29e3e7a28c1915557a02ed93de091ab5e90c219
LunaSaif <143600026+LunaSaif@users.noreply.github.com>
2023-09-11 15:42:22 +0800

    Add files via upload

---
+++ loubnaa.py
@@ -0,0 +1,35 @@
+import dash
+from dash import dcc, html
+import pandas as pd
+import plotly.express as px
+
+app = dash.Dash(__name__)
+server = app.server
+
+data_url = "https://raw.githubusercontent.com/LunaSaif/assignment3/main/COVID-19 Coronavirus.csv"
+data = pd.read_csv(data_url)
+
+app.layout = html.Div([
+    html.H1("Assignment3"),
+    
+    # Add the first bar chart
+    dcc.Graph(
+        id='total-cases-bar-chart',
+        figure=px.bar(data, x='Country', y='Total Cases', title='Total Cases by Country'),
+    ),
+    
+    # Add the second bar chart
+    dcc.Graph(
+        id='total-deaths-bar-chart',
+        figure=px.bar(data, x='Country', y='Total Deaths', title='Total Deaths by Country'),
+    ),
+    
+    # Add the pie chart
+    dcc.Graph(
+        id='cases-pie-chart',
+        figure=px.pie(data, values='Total Cases', names='Continent', title='Cases of Covid-19 by Continent'),
+    ),
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
